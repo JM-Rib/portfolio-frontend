6e4a8571871430a1112727dd70d2099f51a8d6a4
JM_Rib <jeanmarin.ribaric@gmail.com>
2024-06-08 10:56:44 +0200

    ajustement taille max latérale de la page

--- src/components/PartyPopper.js
+++ src/components/PartyPopper.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
+import { Box, Heading } from '@chakra-ui/react';
 
 export default function PartyPopper() {
   const party = useLoader(GLTFLoader, '/party_popper.gltf');
@@ -11,12 +12,10 @@ export default function PartyPopper() {
   const [prevScrollY, setPrevScrollY] = useState(0);
   const requestRef = useRef();
 
-  // Define a maximum rotation threshold in radians
-  const maxRotation = Math.PI / 8; // 45 degrees in radians
-  // Define a decay factor to reduce rotation gradually
-  const decayFactor = 0.92; // Reduces rotation by 5% each frame
+  const maxRotation = Math.PI / 4; // Maximum rotation in radians (45 degrees)
+  const decayFactor = 0.95; // Decay factor to gradually reset rotation
 
-  // Function to clamp the rotation value within the max threshold
+  // Function to clamp rotation within the specified maximum threshold
   const clampRotation = (rotation) => {
     return Math.max(-maxRotation, Math.min(maxRotation, rotation));
   };
@@ -38,22 +37,24 @@ export default function PartyPopper() {
   }, [window.scrollY]);
 
   return (
-    <Canvas style={{ width: '100vw', height: '100vh', left: '8%' }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[1.3, 1.3, 1.3]} intensity={9} />
-      <primitive
-        object={party.scene}
-        position={[-1, 0, -2]}
-        scale={[2.7, 2.8, 2.3]}
-        rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
-      />
-      <pointLight position={[-1, 1.3, -1]} intensity={4} />
-      <primitive
-        object={bottle.scene}
-        position={[1, 0, 1]}
-        scale={[2, 2, 2]}
-        rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
-      />
-    </Canvas>
+    <Box position="relative" width="100%" height="100vh" overflow="hidden">
+      <Canvas style={{ position: 'absolute', top: 0, left: "8%", width: '100%', height: '100%' }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[1.3, 1.3, 1.3]} intensity={9} />
+        <primitive
+          object={party.scene}
+          position={[-1, 0, -2]}
+          scale={[2.7, 2.8, 2.3]}
+          rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
+        />
+        <pointLight position={[-1, 1.3, -1]} intensity={4} />
+        <primitive
+          object={bottle.scene}
+          position={[1, 0, 1]}
+          scale={[2, 2, 2]}
+          rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
+        />
+      </Canvas>
+    </Box>
   );
 }
