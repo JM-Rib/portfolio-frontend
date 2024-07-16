a17ae42e61c2d06e2236b80be80fc87d0ba188f2
JM_Rib <jeanmarin.ribaric@gmail.com>
2024-07-17 00:34:48 +0200

    style changes landing

--- src/components/LandingDiorama.js
+++ src/components/LandingDiorama.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import { OrthographicCamera } from '@react-three/drei';
 
-export default function LandingDiorama({width}) {
+export default function LandingDiorama({ width }) {
   const party = useLoader(GLTFLoader, `/party_popper.gltf`);
   const bottle = useLoader(GLTFLoader, `/bottle_with_popping_cork.gltf`);
 
-  const [scaleFactor, setScaleFactor] = useState(0);
   const [scrollAcceleration, setScrollAcceleration] = useState(0);
   const [prevScrollY, setPrevScrollY] = useState(0);
   const requestRef = useRef();
 
   const maxRotation = Math.PI / 10; // Maximum rotation in radians (45 degrees)
   const decayFactor = 0.95; // Decay factor to gradually reset rotation
-    
+
   const minWidth = 320; // Minimum width (e.g., mobile)
   const maxWidth = 1280; // Maximum width (e.g., large desktop)
 
@@ -34,58 +34,59 @@ export default function LandingDiorama({width}) {
 
     requestRef.current = requestAnimationFrame(animate);
   };
-    
+
   const lerp = (start, end, factor) => start + (end - start) * factor;
 
   useEffect(() => {
     requestRef.current = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(requestRef.current);
   }, [window.scrollY]);
-  
+
   // Normalize width to a factor between 0 (minWidth) and 1 (maxWidth)
   const normalizedWidth = Math.min(Math.max((width - minWidth) / (maxWidth - minWidth), 0), 1);
 
   // Interpolate positions and scales based on normalizedWidth
-  let bottlePosition = [
-    lerp(-0.5, 1, normalizedWidth), // X
+  const bottlePosition = [
+    lerp(-0.5, 4, normalizedWidth), // X
     lerp(2, 0, normalizedWidth),     // Y
     lerp(-0.5, 1, normalizedWidth)    // Z
   ];
 
-  let bottleScale = [
-    lerp(1.7, 2.3, normalizedWidth), 
-    lerp(1.7, 2.3, normalizedWidth), 
+  const bottleScale = [
+    lerp(1.7, 2.3, normalizedWidth),
+    lerp(1.7, 2.3, normalizedWidth),
     lerp(1.4, 2.3, normalizedWidth)
   ];
 
-  let partyPosition = [
-    lerp(0.3, -1, normalizedWidth), // X
-    lerp(0.2, 0, normalizedWidth),   // Y
+  const partyPosition = [
+    lerp(-0.8, 1.8, normalizedWidth), // X
+    lerp(2.2, 1, normalizedWidth),   // Y
     lerp(1.2, -2, normalizedWidth)    // Z
   ];
 
-  let partyScale = [
-    lerp(1, 3, normalizedWidth), 
-    lerp(1, 3.1, normalizedWidth), 
-    lerp(1, 2.6, normalizedWidth)
+  const partyScale = [
+    lerp(1, 2.2, normalizedWidth),
+    lerp(1, 1.9, normalizedWidth),
+    lerp(1, 1.8, normalizedWidth)
   ];
 
-//  let partyLightPos = [
-//    lerp(0.9, 0.9, normalizedWidth), 
-//    lerp(1, 1, normalizedWidth), 
-//    lerp(2.1, 2.1, normalizedWidth)
-//  ];
-
   return (
-    <Box position="relative" width="100%" height="100%" overflow="hidden" bottom="8%">
-      <Canvas style={{ position: 'absolute', top: 0, left: "8%", width: '100%', height:"100%" }}>
-      <ambientLight intensity={0.5} />
-        <pointLight position={[1.3, 1.3, 1.3]} intensity={9} />
+    <Box position="relative" width="100%" height="100%" overflow="hidden" >
+      <Canvas style={{ position: 'absolute', top: 0, left: 0, width: '100%', height:"100%" }}>
+        <OrthographicCamera
+          makeDefault
+          position={[0, 0, 10]}
+          zoom={100}
+          near={0.1}
+          far={100}
+        />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[2, 1.5, -2.3]} intensity={9} />
         <primitive
           object={party.scene}
           position={partyPosition}
           scale={partyScale}
-          rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
+          rotation={[clampRotation(scrollAcceleration), -0.25, -0.1]} // Apply clamped rotation
         />
         <pointLight position={[-1, 1.3, -1]} intensity={4} />
         <primitive
@@ -98,21 +99,3 @@ export default function LandingDiorama({width}) {
     </Box>
   );
 }
-
-
-
-//         <ambientLight intensity={0.5} />
-//         <pointLight position={[1.3, 1.3, 1.3]} intensity={9} />
-//         <primitive
-//           object={party.scene}
-//           position={[-1, 0, -2]}
-//           scale={[3, 3.1, 2.6]}
-//           rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
-//         />
-//         <pointLight position={[-1, 1.3, -1]} intensity={4} />
-//         <primitive
-//           object={bottle.scene}
-//           position={[1, 0, 1]}
-//           scale={[2.3, 2.3, 2.3]}
-//           rotation={[clampRotation(scrollAcceleration), 0, 0]} // Apply clamped rotation
-//         />
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -6,23 +6,29 @@ import LargeEmoji from '../components/LargeEmoji';
 import LandingDiorama from '../components/LandingDiorama';
 import { useAuth } from '../providers/AuthProvider';
 import { APP_ROUTES } from '../utils/constants';
-import { Box, Heading, Container, Stack } from '@chakra-ui/react';
+import { Text, Box, Heading, Container, Stack, Flex, Button, Link as ChakraLink } from '@chakra-ui/react';
 
 function Accueil({width}) {
   const {logout, hasLoginData} = useAuth();
 
 	return (
     <div className="accueil">
-      <Box position="relative" width="100%" height={["70vh", "80vh", "92vh", "92vh"]} bottom="20%" >
-        <LandingDiorama width={width}/>
-        <Box position="absolute" top="0" left="20%" right="0" bottom="20%" display="flex" flexDirection="column" alignItems="left" justifyContent="center" color="white" zIndex="10">
-          <Heading size='md' fontFamily="title" fontSize="7xl" textAlign="left">Félicitations!</Heading>
-          <Heading size='md' fontFamily="title" fontSize="7xl" textAlign="left">Vous êtes arrivé sur mon site!</Heading>
+      <Box position="relative" width="100%" height={["70vh", "80vh", "92vh", "92vh"]} >
+        <LandingDiorama width={width} />
+        <Box position="absolute" top="0" left={["7%","7%","7%","20%"]} right="0" bottom="20%" display="flex" flexDirection="column" alignItems="left" justifyContent="center" color="white" zIndex="10">
+          <Heading size='md' fontFamily="title" fontSize={["4xl","5xl","6xl","7xl"]} textAlign="left">Votre voix compte!</Heading>
+          <Heading size='md' fontFamily="title" fontSize={["4xl","5xl","6xl","7xl"]} textAlign="left">N'agissez pas au dernier moment</Heading>
+          <Heading size='md' mt="4" fontFamily="heading" fontSize={["md","xl","2xl","2xl"]} textAlign="left">Nous vous proposons des mails de rappel pour aller voter le 30 Juin et 7 Juillet 2024</Heading>
+          <Box display="flex" mt="4" fontSize={["sm","sm","md","xl"]} alignItems="left">
+            <ChakraLink href="/inscription" _hover={{ textDecoration: 'none' }}>
+              <Button colorScheme="purple" >
+                Je m'inscris
+              </Button>
+            </ChakraLink>
+          </Box>
         </Box>
       </Box>
       <ProjetCards />
-      <br />
-      Made with <TextEmoji emoji="red_heart" />  in Brest, France <TextEmoji emoji="flag_france" />      
     </div>
     );
 }
