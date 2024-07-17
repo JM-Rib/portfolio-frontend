ebf2d8d092fcc42c7f5cd0b90ac05abd967fe7c3
JM_Rib <jeanmarin.ribaric@gmail.com>
2024-07-17 23:38:04 +0200

    basic landing diorama mobile light2 et 3

--- src/components/LandingDiorama.js
+++ src/components/LandingDiorama.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import { OrthographicCamera } from '@react-three/drei';
 
 export default function LandingDiorama({ width }) {
-  const party = useLoader(GLTFLoader, `/party_popper.gltf`);
-  const bottle = useLoader(GLTFLoader, `/bottle_with_popping_cork.gltf`);
+  const party = useLoader(GLTFLoader, '/party_popper.gltf');
+  const bottle = useLoader(GLTFLoader, '/bottle_with_popping_cork.gltf');
 
   const [scrollAcceleration, setScrollAcceleration] = useState(0);
   const [prevScrollY, setPrevScrollY] = useState(0);
@@ -19,7 +19,6 @@ export default function LandingDiorama({ width }) {
   const minWidth = 320; // Minimum width (e.g., mobile)
   const maxWidth = 1280; // Maximum width (e.g., large desktop)
 
-  // Function to clamp rotation within the specified maximum threshold
   const clampRotation = (rotation) => {
     return Math.max(-maxRotation, Math.min(maxRotation, rotation));
   };
@@ -29,7 +28,6 @@ export default function LandingDiorama({ width }) {
     setScrollAcceleration((prev) => clampRotation(prev + deltaY * 0.009));
     setPrevScrollY(window.scrollY);
 
-    // Apply decay to gradually reset the rotation
     setScrollAcceleration((prev) => prev * decayFactor);
 
     requestRef.current = requestAnimationFrame(animate);
@@ -42,54 +40,68 @@ export default function LandingDiorama({ width }) {
     return () => cancelAnimationFrame(requestRef.current);
   }, [window.scrollY]);
 
-  // Normalize width to a factor between 0 (minWidth) and 1 (maxWidth)
   const normalizedWidth = Math.min(Math.max((width - minWidth) / (maxWidth - minWidth), 0), 1);
 
-  // Interpolate positions and scales based on normalizedWidth
   const bottlePosition = [
     lerp(0.2, 3.9, normalizedWidth), // X
-    lerp(1.3, -0.1, normalizedWidth),     // Y
-    lerp(-0.5, 4, normalizedWidth)    // Z
+    lerp(1.3, -0.1, normalizedWidth), // Y
+    lerp(-0.5, 4, normalizedWidth), // Z
   ];
 
   const bottleScale = [
     lerp(1.3, 2.7, normalizedWidth),
     lerp(1.3, 2.7, normalizedWidth),
-    lerp(1.0, 2.7, normalizedWidth)
+    lerp(1.0, 2.7, normalizedWidth),
   ];
 
   const partyPosition = [
     lerp(-0.7, 1.7, normalizedWidth), // X
-    lerp(1.8, 1, normalizedWidth),   // Y
-    lerp(-1.2, -2, normalizedWidth)    // Z
+    lerp(1.8, 1, normalizedWidth), // Y
+    lerp(-1.2, -2, normalizedWidth), // Z
   ];
 
   const partyScale = [
     lerp(1, 2, normalizedWidth),
     lerp(1, 1.9, normalizedWidth),
-    lerp(1, 1.8, normalizedWidth)
+    lerp(1, 1.8, normalizedWidth),
   ];
 
+  const light1Position = [
+    lerp(-0.6,  1.8,  normalizedWidth), // X
+    lerp(3.0, 2.2,  normalizedWidth), // Y
+    lerp( 0.3, -0.5, normalizedWidth), // Z
+  ];
+
+  const light2Position = [
+    lerp( -1, 0.7, normalizedWidth), // X
+    lerp( 0.8, -1.3, normalizedWidth), // Y
+    lerp( -0.8, -1.5, normalizedWidth), // Z
+  ];
+
+  const light3Position = [
+    lerp(0.6,  5,  normalizedWidth), // X
+    lerp(1.8, 1.5,normalizedWidth), // Y
+    lerp(1.3, 6.2,normalizedWidth), // Z
+  ];
+
+  const light1Intensity = lerp(2, 4, normalizedWidth);
+  const light2Intensity = lerp(2, 4, normalizedWidth);
+  const light3Intensity = lerp(16, 28, normalizedWidth);
+
   return (
-    <Box position="relative" width="100%" height="100%" bottom={["3%", "3%", "7%", "7%"]} overflow="hidden" >
-      <Canvas style={{ position: 'absolute', top: 0, left: 0, width: '100%', height:"100%" }}>
-        <OrthographicCamera
-          makeDefault
-          position={[0, 0, 10]}
-          zoom={100}
-          near={0.1}
-          far={100}
-        />
+    <Box position="relative" width="100%" height="100%" bottom={["3%", "3%", "7%", "7%"]} overflow="hidden">
+      <Canvas style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={100} near={0.1} far={100} />
         <ambientLight intensity={0.5} />
-        <pointLight position={[1.8, 2.2, -0.5]} intensity={4} />
-        <pointLight position={[0.7, -1.3, -1.5]} intensity={4} />
+        <pointLight position={light1Position} intensity={light1Intensity} />
+        <pointLight position={light2Position} intensity={light2Intensity} />
         <primitive
           object={party.scene}
           position={partyPosition}
           scale={partyScale}
           rotation={[clampRotation(scrollAcceleration), -0.25, -0.1]} // Apply clamped rotation
         />
-        <pointLight position={[5, 1.5, 6.2]} intensity={28} />
+        <pointLight position={light3Position} intensity={light3Intensity} />
         <primitive
           object={bottle.scene}
           position={bottlePosition}
